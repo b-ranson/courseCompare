7568707c55eb03a212935715177a9cf511b46fcb
Branson <bransonmbailey04@gmail.com>
2024-02-08 17:38:22 -0500

    more djang setup

--- app/views.py
+++ app/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def home(request):
-   return HttpResponse("Course Compare")
+#def home(request):
+#   return HttpResponse("Course Compare")
+
+def index(request):
+   return render(request, 'index.html')
