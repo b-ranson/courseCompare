d1547202410525f0aba2af16d394d5cd803b5266
b-ranson <bransonmbailey04@gmail.com>
2024-02-08 18:53:41 -0500

    setting up for vercel and getting login page linked

--- courseCompare/settings.py
+++ courseCompare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4$zo^w7v42rt02#7qw1u1=bc%kg=1!5bxq91&3!!ij3)66j5d*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'courseCompare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
