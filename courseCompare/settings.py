de4d4801e793cdf5678f35c61f8aecc3993bca30
Branson <bransonmbailey04@gmail.com>
2024-02-09 02:37:40 +0000

    running on pythonanywhere

--- courseCompare/settings.py
+++ courseCompare/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-4$zo^w7v42rt02#7qw1u1=bc%kg=1!5bxq91&3!!ij3)66j5d*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'bransonb.pythonanywhere.com']
 
 
 # Application definition
@@ -111,7 +112,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
+#MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+#MEDIA_URL='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
