3404d17b94203a63c11e2b949fb56fff368ee4ed
b-ranson <bransonmbailey04@gmail.com>
2024-02-16 16:47:23 -0500

    added login functionality with sqlite3 db for temp usage

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.home, name='home.html'),
+    path('advancedratings', views.advancedratings, name="advancedratings.html"),
+    path('schedulepage', views.schedulepage, name='schedulepage.html'),
+#    path('login', views.login, name="login.html"),
+
+    # Django Auth
+    path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login')
 ]
+
+
+# path('accounts/', include('django.contrib.auth.urls')),
--- main/views.py
+++ main/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def index(request):
-   return render(request, 'index.html', {})
+def home(request):
+   print(request.user)
+   return render(request, 'home.html', {})
+
+#def login(request):
+#   return render(request, 'login.html', {})
+
+def schedulepage(request):
+   return render(request, 'schedulepage.html', {})
+
+def advancedratings(request):
+   return render(request, 'advancedratings.html', {})
