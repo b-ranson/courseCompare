500aa94cc0495cfaa67eca44c57035fd291d10be
b-ranson <bransonmbailey04@gmail.com>
2024-02-16 18:35:27 -0500

    added more login functionality. Prevent from seeing dashboard without being logged in

--- main/views.py
+++ main/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 
 def home(request):
    return render(request, 'home.html', {})
 
-#def login(request):
-#   return render(request, 'login.html', {})
-
+@login_required(login_url='/accounts/login')
 def schedulepage(request):
-   username = request.user
-   return render(request, 'schedulepage.html', {'name': username})
+   return render(request, 'schedulepage.html', {})
 
+@login_required(login_url='/accounts/login')
 def advancedratings(request):
    return render(request, 'advancedratings.html', {})
