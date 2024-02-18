3f09081a3879940bcd24519fe85264905c034756
b-ranson <bransonmbailey04@gmail.com>
2024-02-18 00:12:16 -0500

    set up dynamic display for schedulePage

--- main/views.py
+++ main/views.py
@@ -8,8 +8,20 @@ def home(request):
 
 @login_required(login_url='/accounts/login')
 def schedulepage(request):
-   return render(request, 'schedulepage.html', {})
+   # db logic here, or mode to model functions? need to look into more
+   # will hard code for right now to update template with jinja syntax
+
+   courses = [
+      {'courseName': 'Operating Systems', 'courseID':'COP4610', 'courseRating': 1.2},
+      {'courseName': 'Data Structures II', 'courseID':'COP4530', 'courseRating': 4.8}
+   ]
+
+   context = {'courses': courses}
+   return render(request, 'schedulepage.html', context)
 
 @login_required(login_url='/accounts/login')
 def advancedratings(request):
+
+
+
    return render(request, 'advancedratings.html', {})
