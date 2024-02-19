0593613abd3a7ec45bad8f2e7768aed48c96460c
b-ranson <bransonmbailey04@gmail.com>
2024-02-18 22:56:19 -0500

    set up advanced ratings for dynamic info passing. once again hardcoded db stuff for now. considering adding a user-review sections since there is a lot of free room on the screen

--- main/views.py
+++ main/views.py
@@ -25,10 +25,36 @@ def advancedratings(request, className):
    #make db querey here based on className to pull advancedRatings
    # will hard code what table should look like for now
 
-      
+   # course name and ID, prof, section (this might be too tax to implement)
+   # exam dif, hw dif, lecture dif, workload, total diff (all floats 0-5, prob averaged by integer inputs)
+
+   #className gets passed in, then need to read db and pass vv this stuff
+   # then need to pull advanced ratings from same class name and pass it
+
+
+   courseOS = [
+      {'courseName': 'Operating Systems', 'courseID':'COP4610', 'prof': 'Andy Wang', 'courseSection': '005'},
+   ]
+   advancedRatingsOS = [
+      {'examDif': 2.3, 'hwDif': 3.7, 'lectDif': 2.8, 'workload': 4.8, 'avg': round((2.3 + 3.7 + 2.8 + 4.8)/5.0, 1)},
+   ]
+
+   courseDSII = [
+      {'courseName': 'Data Structures II', 'courseID':'COP4530', 'prof': 'Bob Myers', 'courseSection': '002'},
+   ]
+   advancedRatingsDSII = [
+      {'examDif': 4.6, 'hwDif': 2.8, 'lectDif': 3.3, 'workload': 4.1, 'avg': round((4.6 + 2.8 + 3.3 + 4.1)/5.0, 1)},
+   ]
+
+
+   context = {}
+   if className == 'Operating Systems':
+      context = { 'courses': courseOS, 'advRatings': advancedRatingsOS, 'className': className }
+   elif className == 'Data Structures II':
+      context = { 'courses': courseDSII, 'advRatings': advancedRatingsDSII, 'className': className }
 
    if className is not None:
-      return render(request, 'advancedratings.html', {'className': className})
+      return render(request, 'advancedratings.html', context)
    else:
       return HttpResponse("Not Available")
       
