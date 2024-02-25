37476f34ef7759ea27df96d99c364d642fdcd339
Joshua Belousek <jbelousek2002@gmail.com>
2024-02-25 18:59:21 -0500

    database running
    must use command in notes.txt to get server

--- courseCompare/settings.py
+++ courseCompare/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'courseCompare.wsgi.application'
 
 DATABASES = {
      'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'coursecompare',
+        'USER': 'root',
+        'PASSWORD': 'Jb162814',
+        'HOST': 'localhost',
+        'PORT':'3306'
+        
      }
 }
 
