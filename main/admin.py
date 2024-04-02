fed01d919486038e50e6eff0b3d69993d9a39c6b
Joshua Belousek <jbelousek2002@gmail.com>
2024-04-02 11:32:13 -0400

    changed role base access

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import GroupAdmin
+from .models import CustomGroup
 
 # Register your models here.
 
@@ -6,3 +9,10 @@ from .models import Courses, MyCustomUser, CourseTaking
 admin.site.register(MyCustomUser)
 admin.site.register(Courses)
 admin.site.register(CourseTaking)
+
+
+#group = Group(name='BASEUSER')
+#g1 = Group(name='PAIDUSER')
+#group.save()
+#g1.save()
+
