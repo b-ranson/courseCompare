754c830554d3ab0797934e18b00e7ed5f19dafaa
Joshua Belousek <jbelousek2002@gmail.com>
2024-04-01 22:30:18 -0400

    added more database functionality
    added more database functionality

--- main/decorators.py
+++ main/decorators.py
@@ -15,9 +15,9 @@ def allowed_user(allowed_roles=[]):
     def dec(view_func):
         def wrapper_func(request, *args, **kwargs):
 
-            group = None
-            if request.user.groups.exists():
-                group = request.user.groups.all()[0].name
+            group = request.user.type
+            #if request.user.type.exists():
+                #group = request.user.groups.all()[0].name
             
             if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
